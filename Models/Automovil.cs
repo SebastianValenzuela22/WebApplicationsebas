f85142fbcf78dc6935748bed9a02a89a4a1af55d
PC01-1H <PC01-1H@PC23-1H>
2024-02-20 15:26:49 -0600

    Agregar archivos de proyecto.

---
+++ Models/Automovil.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationsebas.Models
+{
+    public class Automovil
+    {
+        [Table("Automoviles")]
+
+        public class Automovil
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            public string NombreConductor { get; set; }
+
+            public int AñoExpiraLicencia { get; set; }
+
+            public string Foto { get; set; }
+
+
+
+        }
+    }
+}
---
+++ Models/Conductor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApplicationsebas.Models
+{
+    [Table("Conductores")]
+
+    public class Conductor
+    {
+        [Key , DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string NombreConductor { get; set; }
+
+        public int AñoExpiraLicencia { get; set; }
+
+
+        
+
+    }
+}
